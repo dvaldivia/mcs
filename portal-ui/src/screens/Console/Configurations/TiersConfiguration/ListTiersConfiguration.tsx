85c8109f11d512d239f5f9825fef68cbb5905213
Benjamin Perez <benjamin@bexsoft.net>
2021-02-11 19:01:42 -0600

    Added missing columns to tiers list

--- portal-ui/src/screens/Console/Configurations/TiersConfiguration/ListTiersConfiguration.tsx
+++ portal-ui/src/screens/Console/Configurations/TiersConfiguration/ListTiersConfiguration.tsx
@@ -124,6 +124,56 @@ const ListTiersConfiguration = ({
     setIsLoading(true);
   };
 
+  const renderTierName = (item: ITierElement) => {
+    const name = get(item, `${item.type}.name`, "");
+
+    if(name !== null) {
+      return name;
+    }
+
+    return "";
+  };
+
+  const renderTierPrefix = (item: ITierElement) => {
+    const prefix = get(item, `${item.type}.prefix`, "");
+
+    if(prefix !== null) {
+      return prefix;
+    }
+
+    return "";
+  };
+
+  const renderTierEndpoint = (item: ITierElement) => {
+    const endpoint = get(item, `${item.type}.endpoint`, "");
+
+    if(endpoint !== null) {
+      return endpoint;
+    }
+
+    return "";
+  };
+
+  const renderTierBucket = (item: ITierElement) => {
+    const bucket = get(item, `${item.type}.bucket`, "");
+
+    if(bucket !== null) {
+      return bucket;
+    }
+
+    return "";
+  };
+
+  const renderTierRegion = (item: ITierElement) => {
+    const region = get(item, `${item.type}.region`, "");
+
+    if(region !== null) {
+      return region;
+    }
+
+    return "";
+  };
+
   return (
     <Fragment>
       <Grid container>
@@ -173,9 +223,39 @@ const ListTiersConfiguration = ({
                           itemActions={[]}
                           columns={[
                             {
+                              label: "Tier Name",
+                              elementKey: "type",
+                              renderFunction: renderTierName,
+                              renderFullObject: true,
+                            },
+                            {
                               label: "Type",
                               elementKey: "type",
                               width: 150,
+                            },
+                            {
+                              label: "Endpoint",
+                              elementKey: "type",
+                              renderFunction: renderTierEndpoint,
+                              renderFullObject: true,
+                            },
+                            {
+                              label: "Bucket",
+                              elementKey: "type",
+                              renderFunction: renderTierBucket,
+                              renderFullObject: true,
+                            },
+                            {
+                              label: "Prefix",
+                              elementKey: "type",
+                              renderFunction: renderTierPrefix,
+                              renderFullObject: true,
+                            },
+                            {
+                              label: "Region",
+                              elementKey: "type",
+                              renderFunction: renderTierRegion,
+                              renderFullObject: true,
                             },
                           ]}
                           isLoading={isLoading}
